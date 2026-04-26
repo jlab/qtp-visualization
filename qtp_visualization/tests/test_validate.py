979afdb0a01e7881c19dbf8cc74d4321344b5e0e
Stefan Janssen <stefan.m.janssen@gmail.com>
2026-04-26 21:58:02 +0200

    add a bad artifact, where not only the metadata is missing

--- qtp_visualization/tests/test_validate.py
+++ qtp_visualization/tests/test_validate.py
@@ -29,7 +29,7 @@ class ValidateTests(PluginTestCase):
         path_builder = partial(join, self.base_data_dir,
                                dirname(__file__), 'test_data')
         self.valid_qzv = path_builder('good_vis.qzv')
-        self.invalid_qzv = path_builder('bad_vis.qzv')
+        self.invalid_qzv = path_builder('really_bad_vis.qzv')
 
         # Debugging
         sleep(5)
@@ -60,7 +60,7 @@ class ValidateTests(PluginTestCase):
         import sys
         print("STEFAN: prior invalid test", file=sys.stderr)
         invalid_qzv = self.qclient.push_file_to_central(self.invalid_qzv)
-        invalid_qzv = '/home/runner/work/qtp-visualization/qtp-visualization/qtp_visualization/tests/test_data/bad_vis.qzv'
+        invalid_qzv = '/home/runner/work/qtp-visualization/qtp-visualization/qtp_visualization/tests/test_data/really_bad_vis.qzv'
         print("STEFAN: pushed file %s to %s" % (self.invalid_qzv, invalid_qzv), file=sys.stderr)
         obs_succes, obs_ainfo, obs_error = _validate_q2_visualization(
             {'qzv': [invalid_qzv]}, self.out_dir)
